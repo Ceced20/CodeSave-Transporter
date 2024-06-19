690931b3b125b4ce5af85803f668dac210a07a8c
eko-wi <kodosapiens@gmail.com>
2024-06-19 17:25:04 +0700

    Update TransporterTransmit.ino
    perbaiki pembacaan joystick

--- TransporterTransmit.ino
+++ TransporterTransmit.ino
@@ -15,12 +15,19 @@ int CenterX1;
 int CenterY1;
 int CenterX2;
 int CenterY2;
+int deadband=40;
 // State variables for joystick positions
 int last_x_value1 = 512;
 int last_y_value1 = 512;
 int last_x_value2 = 512;
 int last_y_value2 = 512;
 int b3state = 0;
+long t = 0, tlastsend = 0;
+int interval = 50;
+int xValue1;
+int yValue1;
+int xValue2;
+int yValue2;
 // Receiver MAC Address
 uint8_t broadcastAddress[] = { 0xCC, 0x7B, 0x5C, 0x26, 0xCC, 0xB4 };
 
@@ -39,7 +46,7 @@ struct_message joystickData;
 // Math power forward
 int mapToPower(int value, int maxpower) {
   int midpoint = 2048;
-  float powerRange = maxpower;
+  if(abs(value-midpoint)<deadband) return 0;
   float scaledValue = ((float)value - midpoint) / midpoint;
   return (int)(scaledValue * maxpower);
 }
@@ -96,117 +103,147 @@ void setup() {
     Serial.println("Failed to add peer");
     return;
   }
-   CenterX1 = analogRead(joystick1_x);
-   CenterY1 = analogRead(joystick1_y);
-   CenterX2 = analogRead(joystick2_x);
-   CenterY2 = analogRead(joystick2_y);
+  CenterX1 = 0;
+  CenterY1 = 0;
+  CenterX2 = 0;
+  CenterY2 = 0;
+  delay(100);
+  for (int i = 0; i < 16; i++) {
+    CenterX1 += analogRead(joystick1_x);
+    CenterY1 += analogRead(joystick1_y);
+    CenterX2 += analogRead(joystick2_x);
+    CenterY2 += analogRead(joystick2_y);
+    delay(1);
+  }
+  CenterX1 /= 16;
+  CenterY1 /= 16;
+  CenterX2 /= 16;
+  CenterY2 /= 16;
+  xValue1 = 2048;
+  yValue1 = 2048;
+  xValue2 = 2048;
+  yValue2 = 2048;
 }
 
 void loop() {
   // Read joystick values
-  int xValue1 = analogRead(joystick1_x)-(CenterX1-2048);
-  int yValue1 = 4095 - (analogRead(joystick1_y)-(CenterY1-2048));
-  int xValue2 = analogRead(joystick2_x)-(CenterX2-2048);
-  int yValue2 = 4095 - (analogRead(joystick2_y)-(CenterY2-2048));
-
-  // Map joystick values to power
-  int scaledValueX = mapToPower(xValue1, 125);
-  int scaledValueY = mapToPower(yValue1, 125);
-  joystickData.M1 = (scaledValueX / 2) + scaledValueY;
-  joystickData.M2 = (-scaledValueX / 2) + scaledValueY;
-  joystickData.M1 = constrain(joystickData.M1, -100, 100);
-  joystickData.M2 = constrain(joystickData.M2, -100, 100);
-  if (digitalRead(button1) == LOW) {
-    joystickData.servoA = 180;
-  }
-
-  if (digitalRead(button2) == LOW) {
-    joystickData.servoA = 0;
-  }
-
-  if (digitalRead(button3) == LOW) {
-    if (b3state == 0) {  //sebelumnya tidak ditekan
-      b3state = 1;       //catat keadaan sekarang supaya tidak mengulang
-      if (joystickData.servoC == 140) {
-        joystickData.servoC = 20;
-      } else {
-        joystickData.servoC = 140;
-      }
+  t = millis();
+  int newxValue1 = analogRead(joystick1_x) - (CenterX1 - 2048);
+  delayMicroseconds(50);
+  int newyValue1 = 4095 - (analogRead(joystick1_y) - (CenterY1 - 2048));
+  delayMicroseconds(50);
+  int newxValue2 = analogRead(joystick2_x) - (CenterX2 - 2048);
+  delayMicroseconds(50);
+  int newyValue2 = 4095 - (analogRead(joystick2_y) - (CenterY2 - 2048));
+  delayMicroseconds(50);
+  //rata-ratakan nilai baru dengan nilai lama untuk memperkecil fluktuasi
+  xValue1 = (7 * xValue1 + newxValue1) / 8;
+  yValue1 = (7 * yValue1 + newyValue1) / 8;
+  xValue2 = (7 * xValue2 + newxValue2) / 8;
+  yValue2 = (7 * yValue2 + newyValue2) / 8;
+  // proses data untuk dikirim kalau sudah waktunya
+  if (t - tlastsend >= interval) {
+    tlastsend = t;  
+    // Map joystick values to power
+    int scaledValueX = mapToPower(xValue1, 125);
+    int scaledValueY = mapToPower(yValue1, 125);
+    joystickData.M1 = (scaledValueX / 2) + scaledValueY;
+    joystickData.M2 = (-scaledValueX / 2) + scaledValueY;
+    joystickData.M1 = constrain(joystickData.M1, -100, 100);
+    joystickData.M2 = constrain(joystickData.M2, -100, 100);
+    if (digitalRead(button1) == LOW) {
+      joystickData.servoA = 180;
     }
-  } else {  //kalau button tidak ditekan
-    b3state = 0;
-  }
-  //joystick2 mapping value
-
-  scaledValueX = mapToPower(xValue2, 180);
-  scaledValueY = mapToPower(yValue2, 180);
-  if (abs(scaledValueX) < 10 && abs(scaledValueY) < 10) {
-    //joystick center, servo ke 90 derajat
-    joystickData.servoS = 90;
-  } else if (scaledValueY >= 0) {
-    joystickData.servoS = atan2(scaledValueY, -scaledValueX) * 57.29578;
-  } else {
-    joystickData.servoS = atan2(-scaledValueY, -scaledValueX) * 57.29578;
-  }
-
   
-  Serial.print(xValue1);
-  Serial.print(",");
-  Serial.print(yValue1);
-  Serial.print(",");
-  Serial.print(xValue2);
-  Serial.print(",");
-  Serial.print(yValue2);
-  Serial.print(",   ");
-  Serial.print(joystickData.M1);
-  Serial.print(",");
-  Serial.print(joystickData.M2);
-  Serial.print(",");
-  Serial.print(joystickData.servoA);
-  Serial.print(",");
-  Serial.print(joystickData.servoC);
-  Serial.print(", xy=");
-  Serial.print(scaledValueX);
-  Serial.print(",");
-  Serial.print(scaledValueY);
-  Serial.print(",s=");
-
-  Serial.print(joystickData.servoS);
-  Serial.println();
-
+    if (digitalRead(button2) == LOW) {
+      joystickData.servoA = 0;
+    }
   
-
-  /*
-  // Check if joystick 1 has moved significantly
-  if (abs(xValue1 - last_x_value1) > 10 || abs(yValue1 - last_y_value1) > 10) {
-    Serial.print("Joystick 1 moved: x=");
+    if (digitalRead(button3) == LOW) {
+      if (b3state == 0) {  //sebelumnya tidak ditekan
+        b3state = 1;       //catat keadaan sekarang supaya tidak mengulang
+        if (joystickData.servoC == 140) {
+          joystickData.servoC = 20;
+        } else {
+          joystickData.servoC = 140;
+        }
+      }
+    } else {  //kalau button tidak ditekan
+      b3state = 0;
+    }
+    //joystick2 mapping value
+  
+    scaledValueX = mapToPower(xValue2, 180);
+    scaledValueY = mapToPower(yValue2, 180);
+    if (abs(scaledValueX) < 10 && abs(scaledValueY) < 10) {
+      //joystick center, servo ke 90 derajat
+      joystickData.servoS = 90;
+    } else if (scaledValueY >= 0) {
+      joystickData.servoS = atan2(scaledValueY, -scaledValueX) * 57.29578;
+    } else {
+      joystickData.servoS = atan2(-scaledValueY, -scaledValueX) * 57.29578;
+    }
+  
+    
+    Serial.print(xValue1);
+    Serial.print(",");
+    Serial.print(yValue1);
+    Serial.print(",");
+    Serial.print(xValue2);
+    Serial.print(",");
+    Serial.print(yValue2);
+    Serial.print(",   ");
     Serial.print(joystickData.M1);
-    Serial.print(", y=");
-    Serial.println(joystickData.M2);
-    last_x_value1 = xValue1;
-    last_y_value1 = yValue1;
-  }
-
-  // Check if joystick 2 has moved significantly
-  if (abs(xValue2 - last_x_value2) > 10 || abs(yValue2 - last_y_value2) > 10) {
-    Serial.print("Joystick 2 moved: s=");
-    Serial.println(joystickData.servoS);
-    last_x_value2 = xValue2;
-    last_y_value2 = yValue2;
-  }
-*/
-  // Send joystick data via ESP-NOW
-  esp_err_t result = esp_now_send(broadcastAddress, (uint8_t *)&joystickData, sizeof(joystickData));
+    Serial.print(",");
+    Serial.print(joystickData.M2);
+    Serial.print(",");
+    Serial.print(joystickData.servoA);
+    Serial.print(",");
+    Serial.print(joystickData.servoC);
+    Serial.print(", xy=");
+    Serial.print(scaledValueX);
+    Serial.print(",");
+    Serial.print(scaledValueY);
+    Serial.print(",s=");
   
-  if (result == ESP_OK) {
-    ledon();
-    //Serial.println("Sent with success");
-  } else {
-    ledoff();
-    //Serial.println("Error sending the data");
+    Serial.print(joystickData.servoS);
+    Serial.println();
+  
+    
+  
+    /*
+    // Check if joystick 1 has moved significantly
+    if (abs(xValue1 - last_x_value1) > 10 || abs(yValue1 - last_y_value1) > 10) {
+      Serial.print("Joystick 1 moved: x=");
+      Serial.print(joystickData.M1);
+      Serial.print(", y=");
+      Serial.println(joystickData.M2);
+      last_x_value1 = xValue1;
+      last_y_value1 = yValue1;
+    }
+  
+    // Check if joystick 2 has moved significantly
+    if (abs(xValue2 - last_x_value2) > 10 || abs(yValue2 - last_y_value2) > 10) {
+      Serial.print("Joystick 2 moved: s=");
+      Serial.println(joystickData.servoS);
+      last_x_value2 = xValue2;
+      last_y_value2 = yValue2;
+    }
+  */
+    // Send joystick data via ESP-NOW
+    esp_err_t result = esp_now_send(broadcastAddress, (uint8_t *)&joystickData, sizeof(joystickData));
+    
+    if (result == ESP_OK) {
+      ledon();
+      //Serial.println("Sent with success");
+    } else {
+      ledoff();
+      //Serial.println("Error sending the data");
+    }
+  
+    //delay(25);  // Send data at 20Hz
+    //ledoff();
+    //delay(25);
   }
-
-  delay(25);  // Send data at 20Hz
-  ledoff();
-  delay(25);
+  if (t - tlastsend > 10) ledoff();
 }
