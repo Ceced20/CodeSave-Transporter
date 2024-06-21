1bf4ae1a89b606f75d5f92f94ea1bd40203b8349
eko-wi <kodosapiens@gmail.com>
2024-06-21 16:35:56 +0700

    Update transmitter_transporterCed_bt.ino
    tambahkan center joystick begitu connect

--- transmitter_transporterCed_bt.ino
+++ transmitter_transporterCed_bt.ino
@@ -8,14 +8,29 @@
 #define button1 14
 #define button2 13
 #define button3 27
+#define LEDPIN 2
 //tombol-tombol active low
-
+#define SGRIP_ANGLE_HOLD 140
+#define SGRIP_ANGLE_OPEN 0
+#define SANGKAT_UP 0
+#define SANGKAT_DOWN 180
 // State variables for joystick positions
-int last_x_value1 = 512;
-int last_y_value1 = 512;
-int last_x_value2 = 512;
-int last_y_value2 = 512;
+int CenterX1;
+int CenterY1;
+int CenterX2;
+int CenterY2;
+int deadband = 80;
+// State variables for joystick positions
+int lastValueY = 0;
 int b3state = 0;
+long t = 0, tlastsend = 0;
+int interval = 50;
+int yaccel = 50;
+int ydecel = 50;
+int xValue1 = 0;
+int yValue1 = 0;
+int xValue2 = 0;
+int yValue2 = 0;
 // Receiver MAC Address
 uint8_t recvAddress[] = {0xCC, 0x7B, 0x5C, 0x26, 0xCC, 0xB6};
 const char pin[] = "1234";
@@ -23,6 +38,14 @@ String transmitter_btname = "BT-Transmitter";
 bool connected = false;
 
 BluetoothSerial btserial;
+
+void ledon() {
+  digitalWrite(LEDPIN, HIGH);
+}
+
+void ledoff() {
+  digitalWrite(LEDPIN, LOW);
+}
 // Structure to send data
 typedef struct struct_message {
   uint16_t headerbytes;
@@ -37,15 +60,36 @@ typedef struct struct_message {
 struct_message joystickData;
 
 // Math power forward
-int mapToPower(int value, int maxpower) {
-  int midpoint = 2048;
-  float powerRange = maxpower;
-  float scaledValue = ((float)value - midpoint) / midpoint;
-  return (int)(scaledValue * maxpower);
-
+float mapToPower(float value, float maxpower) {
+  float midpoint = 2048;
+  if (fabs(value - midpoint) < deadband) return 0;
+  float scaledValue = (value - midpoint) * maxpower / midpoint;
+  return scaledValue;
 }
 
-
+void center() {
+  //hitung titik tengah dari joystick
+  CenterX1 = 0;
+  CenterY1 = 0;
+  CenterX2 = 0;
+  CenterY2 = 0;
+  delay(100);
+  for (int i = 0; i < 128; i++) {
+    CenterX1 += analogRead(joystick1_x);
+    delayMicroseconds(50);
+    CenterY1 += analogRead(joystick1_y);
+    delayMicroseconds(50);
+    CenterX2 += analogRead(joystick2_x);
+    delayMicroseconds(50);
+    CenterY2 += analogRead(joystick2_y);
+    delayMicroseconds(50);
+    //delay(1);
+  }
+  CenterX1 /= 128;
+  CenterY1 /= 128;
+  CenterX2 /= 128;
+  CenterY2 /= 128;
+}
 void setup() {
   // Init Serial Monitor
   Serial.begin(115200);
@@ -55,17 +99,24 @@ void setup() {
   pinMode(joystick1_y, INPUT);
   pinMode(joystick2_x, INPUT);
   pinMode(joystick2_y, INPUT);
-
-  // Set device as a Wi-Fi Station
-
-
-  // Init ESP-NOW
+  pinMode(LEDPIN, OUTPUT);
+  ledoff();
   Serial.println("init bluetooth...");
   btserial.begin(transmitter_btname, true);
   btserial.setPin(pin);
+  center();
+  xValue1 = 2048;
+  yValue1 = 2048;
+  xValue2 = 2048;
+  yValue2 = 2048;
+  joystickData.servoA = SANGKAT_DOWN;
+  joystickData.servoS = 90;
+  joystickData.servoC = SGRIP_ANGLE_OPEN;
 }
 
 void loop() {
+
+
   //tunggu sampai connect
   if (!connected) {
     Serial.println("connecting...");
@@ -73,89 +124,110 @@ void loop() {
   }
   if (connected) {
     Serial.println("Connected successfully!");
+    center();
   }
   while (btserial.connected()) {
-
-    // Read joystick values
-    int xValue1 = analogRead(joystick1_x);
-    int yValue1 = 4095 - analogRead(joystick1_y);
-    int xValue2 = analogRead(joystick2_x);
-    int yValue2 = 4095 - analogRead(joystick2_y);
-
+    t = millis();
+    //baca posisi joystick
+    int newxValue1 = analogRead(joystick1_x) - (CenterX1 - 2048);
+    delayMicroseconds(50);
+    int newyValue1 = 4095 - (analogRead(joystick1_y) - (CenterY1 - 2048));
+    delayMicroseconds(50);
+    int newxValue2 = analogRead(joystick2_x) - (CenterX2 - 2048);
+    delayMicroseconds(50);
+    int newyValue2 = 4095 - (analogRead(joystick2_y) - (CenterY2 - 2048));
+    delayMicroseconds(50);
+    //rata-ratakan nilai baru dengan nilai lama untuk memperkecil fluktuasi
+    xValue1 = (7 * xValue1 + newxValue1) / 8;
+    yValue1 = (7 * yValue1 + newyValue1) / 8;
+    xValue2 = (7 * xValue2 + newxValue2) / 8;
+    yValue2 = (7 * yValue2 + newyValue2) / 8;
     // Map joystick values to power
-    int scaledValueX = mapToPower(xValue1, 100);
-    int scaledValueY = mapToPower(yValue1, 100);
-    joystickData.M1 = (scaledValueX / 2)  + scaledValueY;
-    joystickData.M2 = (-scaledValueX / 2) + scaledValueY;
-    joystickData.M1 = constrain(joystickData.M1, -100, 100);
-    joystickData.M2 = constrain(joystickData.M2, -100, 100);
-    if (digitalRead(button1) == LOW ) {
-      joystickData.servoA = 0;
-    }
-
-    if (digitalRead(button2) == LOW) {
-      joystickData.servoA = 180;
-    }
-
-    if (digitalRead(button3) == LOW) {
-      if (b3state == 0) { //sebelumnya tidak ditekan
-        b3state = 1; //catat keadaan sekarang supaya tidak mengulang
-        if (joystickData.servoC == 140) {
-          joystickData.servoC = 20;
+    if (t - tlastsend >= interval) {
+      tlastsend = t;
+      // Map joystick values to power
+      int scaledValueX = mapToPower(xValue1, 125);
+      int scaledValueY = mapToPower(yValue1, 125);
+      //batasi laju perubahan kecepatan dengan nilai yaccel
+      if (scaledValueY >= 0) { //gerak maju
+        if (scaledValueY > lastValueY) { //perubahan power positif
+          if (scaledValueY - lastValueY > yaccel) { //perubahannya besar
+            scaledValueY = lastValueY + yaccel;
+          }
         }
-        else {
-          joystickData.servoC = 140;
+        else { //perubahan power negatif, melambat
+          if (lastValueY - scaledValueY > ydecel) {
+            scaledValueY = lastValueY - ydecel;
+          }
         }
       }
-    }
-    else { //kalau button tidak ditekan
-      b3state = 0;
-    }
-    //joystick2 mapping value
-
-    scaledValueX = mapToPower(xValue2, 180);
-    scaledValueY = mapToPower(yValue2, 180);
-    joystickData.servoS = atan2(scaledValueY, -scaledValueX) * 57.29578 + 90;
-    Serial.print(xValue1); Serial.print(",");
-    Serial.print(yValue1); Serial.print(",");
-    Serial.print(xValue2); Serial.print(",");
-    Serial.print(yValue2); Serial.print(",   ");
-    Serial.print(joystickData.M1); Serial.print(",");
-    Serial.print(joystickData.M2); Serial.print(",");
-    Serial.print(joystickData.servoA); Serial.print(",");
-    Serial.print(joystickData.servoC); Serial.print(",");
-    Serial.print(joystickData.servoS); Serial.println();
-
-    /*
-      // Check if joystick 1 has moved significantly
-      if (abs(xValue1 - last_x_value1) > 10 || abs(yValue1 - last_y_value1) > 10) {
-        Serial.print("Joystick 1 moved: x=");
-        Serial.print(joystickData.M1);
-        Serial.print(", y=");
-        Serial.println(joystickData.M2);
-        last_x_value1 = xValue1;
-        last_y_value1 = yValue1;
+      else { //gerak mundur
+        if (scaledValueY < lastValueY) { //makin negatif = makin cepat tapi mundur
+          if (lastValueY - scaledValueY > yaccel) {
+            scaledValueY = lastValueY - yaccel;
+          }
+        }
+        else { //power negatif tapi betambah menuju 0 = mundur melambat
+          if (scaledValueY - lastValueY > ydecel) {
+            scaledValueY = lastValueY + ydecel;
+          }
+        }
+      }
+      lastValueY = scaledValueY;
+      joystickData.M1 = (scaledValueX / 2) + scaledValueY;
+      joystickData.M2 = (-scaledValueX / 2) + scaledValueY;
+      joystickData.M1 = constrain(joystickData.M1, -100, 100);
+      joystickData.M2 = constrain(joystickData.M2, -100, 100);
+      if (digitalRead(button1) == LOW) {
+        joystickData.servoA = SANGKAT_UP;
       }
 
-      // Check if joystick 2 has moved significantly
-      if (abs(xValue2 - last_x_value2) > 10 || abs(yValue2 - last_y_value2) > 10) {
-        Serial.print("Joystick 2 moved: s=");
-        Serial.println(joystickData.servoS);
-        last_x_value2 = xValue2;
-        last_y_value2 = yValue2;
+      if (digitalRead(button2) == LOW) {
+        joystickData.servoA = SANGKAT_DOWN;
       }
-    */
-    // Send joystick data via bluetooth
-    joystickData.headerbytes = 0x2655;
-    //btserial.println(joystickData.M1);
-    btserial.write((uint8_t*) &joystickData, sizeof(joystickData));
-    /*
-      if (result == ESP_OK) {
-        Serial.println("Sent with success");
+
+      if (digitalRead(button3) == LOW) {
+        if (b3state == 0) {  //sebelumnya tidak ditekan
+          b3state = 1;       //catat keadaan sekarang supaya tidak mengulang
+          if (joystickData.servoC == SGRIP_ANGLE_HOLD) {
+            joystickData.servoC = SGRIP_ANGLE_OPEN;
+          } else {
+            joystickData.servoC = SGRIP_ANGLE_HOLD;
+          }
+        }
+      } else {  //kalau button tidak ditekan
+        b3state = 0;
+      }
+      //joystick2 mapping value
+
+      scaledValueX = mapToPower(xValue2, 100);
+      scaledValueY = mapToPower(yValue2, 100);
+      if (abs(scaledValueX) < 20 && abs(scaledValueY) < 20) {
+        //joystick center, servo ke 90 derajat
+        joystickData.servoS = 90;
+      } else if (scaledValueY >= 0) {
+        joystickData.servoS = atan2(scaledValueY, -scaledValueX) * 57.29578;
       } else {
-        //Serial.println("Error sending the data");
+        joystickData.servoS = atan2(-scaledValueY, -scaledValueX) * 57.29578;
       }
-    */
-    delay(50); // Send data at 20Hz
+      Serial.print(xValue1); Serial.print(",");
+      Serial.print(yValue1); Serial.print(",");
+      Serial.print(xValue2); Serial.print(",");
+      Serial.print(yValue2); Serial.print(",   ");
+      Serial.print(joystickData.M1); Serial.print(",");
+      Serial.print(joystickData.M2); Serial.print(",");
+      Serial.print(joystickData.servoA); Serial.print(",");
+      Serial.print(joystickData.servoC); Serial.print(",");
+      Serial.print(joystickData.servoS); Serial.println();
+
+      // Send joystick data via bluetooth
+      joystickData.headerbytes = 0x2655; //yang dikirim byte kecil dulu (litle endian) jadi 0x55 lalu 0x26
+      //btserial.println(joystickData.M1);
+      btserial.write((uint8_t*) &joystickData, sizeof(joystickData));
+
+    }
+    if (t - tlastsend > 10) ledoff();
   }
+  //keluar dari sini berarti putus koneksi
+  connected = false;
 }
